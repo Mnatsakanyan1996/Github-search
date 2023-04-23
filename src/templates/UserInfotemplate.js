4ec3eca45d0b001dd2e3b0bbde678edb2e584737
Mnatsakanyan1996 <m.a.mnatsakanyan@gmail.com>
2023-04-23 15:47:18 +0400

    Hotfix

--- src/templates/UserInfotemplate.js
+++ src/templates/UserInfotemplate.js
@@ -5,8 +5,8 @@ export default (details) => `
   <div class="user-description">
     <h1>${details.name}</h1>
     <h3>${details.login}</h3>
-    <h4>${details.bio}</h4>
-    <h4> <span>&#127757;</span> ${details.location}</h4>
+    <h4>${details.bio || ''}</h4>
+    <h4>${details.location ? '<span>&#127757;</span> ${details.location}' : ''}</h4>
     <div class="subscriptions">
       <span>&#128101;</span>
       <h4>Following ${details.following}</h4>
